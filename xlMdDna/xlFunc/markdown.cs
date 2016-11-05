0e57b5cbdf89a5f5c333ab40d3253c838a04e43e
WaToI <gemini_3ma@hotmail.com>
2016-11-05 22:58:51 +0900

    Ver 0.4.0 Extend Markdown

--- xlMdDna/xlFunc/markdown.cs
+++ xlMdDna/xlFunc/markdown.cs
@@ -1,18 +1,17 @@
 ﻿namespace xlMdDna {
 
 	using ExcelDna.Integration;
-	using Microsoft.Office.Core;
 	using Microsoft.Office.Interop.Excel;
 	using System;
 	using System.Collections.Generic;
-	using System.Drawing;
+	using System.Diagnostics;
 	using System.IO;
 	using System.Reflection;
 	using System.Text;
 	using System.Text.RegularExpressions;
 	using System.Windows.Forms;
 
-	public static class xlMermaid {
+	public static class xlMarkdown {
 		private static DirectoryInfo MyDoc { get { return new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)); } }
 		private static DirectoryInfo saveDir { get { return new DirectoryInfo($@"{MyDoc.FullName}\xlMdDna"); } }
 		private static Microsoft.Office.Interop.Excel.Application xl = (Microsoft.Office.Interop.Excel.Application)ExcelDnaUtil.Application;
@@ -20,16 +19,12 @@
 		private static Worksheet ws;
 		private static Range rng;
 		private static ExcelReference caller;
+		private static IntPtr userWebBrowser;
+		private static IntPtr prevWebBrowser;
 		private static string shapName = "";
 		private static bool initEnd = false;
-		private static Form wind;
-		private static WebBrowser web;
-		private static int width = 400 * 2;
-		private static int height = 400 * 2;
-		private static bool firstTime = true;
-		private static dynamic lastStyle = "zoom:300%;";
-		private static dynamic lastPos = null;
 		private static string md;
+		private static string dq = "\"";
 
 		private static string GetResource(string resourceName) {
 			using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
@@ -39,28 +34,52 @@
 
 		private static string HTML {
 			//get{ return GetResource("xlMdDna.content.def.html"); }
-			get { return @"
+			get {
+				return @"
 <!DOCTYPE html>
 <html lang='ja'>
+
 <head>
 	<meta charset='utf-8'>
 	<meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'>
 	<meta name='viewport' content='width=device-width, initial-scale=1'>
-	<title>xlMdDnaPreview</title>
+	{JQJS}
 	{MDJS}
 	{MMJS}
 	{MMCS}
 </head>
-<body style='background-color: #ffffff;'>
-	<div id='preview' class='mermaid'>
-		{MMSTR}
+
+<body>
+	<div id='md'>
+{MDSTR}
+	</div>
+	<div id='preview'>
 	</div>
 <script>
-	{MMOP}
+var renderer = new marked.Renderer();
+renderer.code = function (code, language) {
+    if(language == 'mermaid')
+        return '<pre class="
++ $"{dq}mermaid{dq}" + @">'+code+'</pre>';
+    else
+        return '<pre><code>'+code+'</code></pre>';
+};
+
+$(document).ready(function(){
+	var md = $('#md').text()
+	$('#md').empty()
+	var html = marked(md ,{ renderer: renderer } );
+	$('#preview').html(html);
+	mermaid.init();
+});
+
+{MMIE}
 </script>
 </body>
+
 </html>
-"; }
+";
+			}
 		}
 
 		private static string JQJS {
@@ -83,12 +102,27 @@
 			get { return @"<link rel='stylesheet' type='text/css' href='https://cdnjs.cloudflare.com/ajax/libs/mermaid/6.0.0/mermaid.min.css'>"; }
 		}
 
-		private static string mmieop {
+		private static string MMIE {
 			get { return @"mermaid.initialize({flowchart:{htmlLabels:false}});"; }
 		}
 
-		[ExcelFunction(Name = "Mermaid", Description = "About xlMdDna")]
-		public static string Mermaid(dynamic[,] args) {
+		private static string MYCSS {
+			get {
+				return @"
+	<style type='text/css'> <!--
+		body { width: 100%; background-color: #ffffff;}
+		table { border-collapse: collapse;  margin: 10px}
+		th { padding: 6px; text-align: left; vertical-align: top; color: #333; background-color: #eee; border: 1px solid #b9b9b9; }
+		td { padding: 6px; background-color: #fff; border: 1px solid #b9b9b9; }
+		.mermaid{ margin-left: 10px; margin-right: auto; }
+	--> </style>
+		}
+";
+			}
+		}
+
+		//[ExcelFunction(Name = "Markdown", Description = "About xlMdDna")]
+		public static string Markdown(dynamic[,] args) {
 			initEnd = init();
 			caller = XlCall.Excel(XlCall.xlfCaller) as ExcelReference;
 			wb = (Workbook)xl.ActiveWorkbook;
@@ -113,52 +147,7 @@
 			if (!initEnd) {
 				if (!saveDir.Exists)
 					saveDir.Create();
-
-				wind = new Form();
-				wind.Text = "press {Enter} to Save";
-				wind.Width = width;
-				wind.Height = height;
-				wind.AutoScaleMode = AutoScaleMode.Font;
-				//wind.AutoSize = true;
-				wind.BackColor = Color.White;
-				wind.TopMost = true;
-				web = new WebBrowser();
-				web.Visible = true;
-				web.Dock = DockStyle.Fill;
-				wind.Controls.Add(web);
-				wind.Show();
-
-				web.DocumentCompleted += (s, e) => {
-					if (firstTime) {
-						firstTime = false;
-						var x = (int)(web.Document.Window.Size.Width / 2 * 2);
-						var y = 0;// (int)(web.Document.Window.Size.Height/2*.5);
-						web.Document.Body.Style = "zoom:350%;";
-						web.Document.Window.ScrollTo(x, y);
-					}
-				};
-
-				web.PreviewKeyDown += (s, e) => {
-					if (e.KeyData == Keys.Enter) {
-						windCapture();
-						wind.Close();
-						web.Dispose();
-					}
-				};
-
-				wind.FormClosing += (s, e) => {
-					firstTime = true;
-					initEnd = false;
-					//windCapture();
-					//e.Cancel = true;
-					//wind.Hide();
-				};
 			}
-			if (!wind.Visible) {
-				wind.Show();
-			}
-			wind.FormBorderStyle = FormBorderStyle.Sizable;
-			web.ScrollBarsEnabled = true;
 
 			return true;
 		}
@@ -193,72 +182,32 @@
 
 		private static void windCapture() {
 			try {
-				wind.FormBorderStyle = FormBorderStyle.None;
-				web.ScrollBarsEnabled = false;
-				lastPos = web.Document.Window.Position;
-				//wind.Activate();
-				saveSvg(shapName + ".svg");
-				SendKeys.SendWait("%{PRTSC}");
-				var bmp = (Bitmap)Clipboard.GetImage();
-				bmp.MakeTransparent(Color.White);
-				//Clipboard.SetImage(bmp);
-				var path = $"{saveDir.FullName}/{shapName}.png";
-				bmp.Save(path);
-				//xl.ActiveSheet.Paste();
-				try {
-					var tshap = ws.Shapes.Item(shapName);
-					tshap.Delete();
-				}
-				catch (Exception) { }
-				try {
-					var shap = ws.Shapes.AddPicture(path, MsoTriState.msoFalse, MsoTriState.msoCTrue, 0f, 0f, bmp.Width, bmp.Height);
-					shap.Name = shapName;
-					shap.Left = float.Parse($"{rng.Offset[0, 1].Left}");
-					shap.Top = float.Parse($"{rng.Top}");
-				}
-				catch (Exception) { }
-				wind.FormBorderStyle = FormBorderStyle.Sizable;
-				web.ScrollBarsEnabled = true;
 			}
 			catch (Exception ex) {
 				MessageBox.Show($"Err: {ex.Message}");
 			}
 		}
 
-		private static string getSvg() {
-			var pv = web.Document.GetElementById("preview");
-			var svgStr = pv.InnerHtml;
-			var dq = "\"";
-			svgStr = Regex.Replace(svgStr, $@"[^\s]*={dq}{dq}", "");
-			svgStr = Regex.Replace(svgStr, $@" *NS\d*:ns\d*:", "");
-			svgStr = Regex.Replace(svgStr, $@"NS[^>]*(/*)>", "$1>");
-			svgStr = Regex.Replace(svgStr, $@"{dq}(space=)", $"{dq} xmlns:$1");
-			svgStr = Regex.Replace(svgStr, $@"<tspan[^>]*><", "<");
-			svgStr = Regex.Replace(svgStr, $@" xml(ns)*:[^ ]*", "");
-			svgStr = Regex.Replace(svgStr, $@"\s+", " ");
-			svgStr = svgStr.Replace("/* */", "");
-			//Clipboard.SetText(svgStr);
-			return svgStr;
-		}
-
-		private static void saveSvg(string fileName = "preview.svg") {
-			File.WriteAllText($"{saveDir.FullName}/{fileName}", getSvg());
-		}
-
-		private static Form getPreviewWindow(string md, string fileName = "preview.html") {
+		private static void getPreviewWindow(string md, string fileName = "preview.html") {
 			var html = HTML
-				.Replace("{MDJS}", MMJS)
+				.Replace("{JQJS}", JQJS)
+				.Replace("{MDJS}", MDJS)
 				.Replace("{MMJS}", MMJS)
 				.Replace("{MMCS}", MMCS)
-				.Replace("{MMOP}", md.StartsWith("graph") ? mmieop : "")
-				.Replace("{MMSTR}", md)
+				.Replace("{MMIE}", MMIE)
+				.Replace("{MDSTR}", md)
 				.Trim();
 
 			var path = $@"{saveDir}\{fileName}";
 			File.WriteAllText(path, html);
 			//web.Navigate(path);
-			web.DocumentText = html;
-			return wind;
+			//web.DocumentText = html;
+			try {
+				var psi = new ProcessStartInfo("explorer.exe", path);
+				var p = Process.Start(psi);
+			}
+			catch (Exception) {
+			}
 		}
 	}
 }
